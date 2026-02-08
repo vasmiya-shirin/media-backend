64e37caec6d9a69cac67a9bb73f43d185f1afc3e
vasmiya shirin <vasmiyak@gmail.com>
2026-02-08 19:51:45 +0300

    change in cors

--- index.js
+++ index.js
@@ -7,7 +7,10 @@ const connectDB = require("./config/db");
 const mediaRoutes = require("./routes/mediaRoutes");
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: "*", // allow all origins for simplicity
+  methods: ["GET", "POST"],
+}));
 app.use(express.json());
 app.use("/uploads", express.static("uploads"));
 
